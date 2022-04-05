d050e20fa08b699c30c504276a89e8e8d0bb844f
youknowznm <scarletaeonia@163.com>
2022-04-05 15:02:21 +0800

    feat: init vite

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-cond-assign': ['error', 'always'],
+    'no-console': 'off',
+    'no-trailing-spaces': ['error'],
+  },
+}
